3a6c5ce528144ed1b3b18c7c1919170dbeba96a7
boulassel <boulasselsamir@gmail.com>
2015-04-07 11:09:08 +0200

    un commit

---
+++ tchat/tchat_message.php
@@ -0,0 +1,6 @@
+<?php
+
+if (array_key_exists('id', $_REQUEST))
+    die(json_encode(newPDO()->prepare('SELECT * FROM messages WHERE id > :id')->execute($_REQUEST)));
+
+newPDO()->prepare('INSERT INTO messages (user,message) VALUES (:user,:message)')->execute($_REQUEST);
